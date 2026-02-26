1f29a4416bb8cf11a1b81f8174378e7de5193aaf
LucasSilveiradaLuz <luzlucas22022007@gmail.com>
2026-02-26 11:34:53 -0300

    feat: getting the data from API

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
- let url = "AIzaSyACsjxeAUT5eI8xeWlu0lBNwy-YEqvtWD8"
+let addbutton = document.getElementById("Adicionar")
+let textInput = document.getElementById("textInput")
+let search = document.getElementById("Pesquisa")
+
+search.addEventListener("click", () => { 
+let query = textInput.value;  
+  if (!query) return;
+  
+
+ let url = `https://www.googleapis.com/books/v1/volumes?q=${query}&keyAIzaSyACsjxeAUT5eI8xeWlu0lBNwy-YEqvtWD8`
 
  fetch(url)
         .then(response => response.json())
         .then(data => {
+     document.getElementById("Title").innerText = "";
+      document.getElementById("Author").innerText = "";
+      document.getElementById("Description").innerText = "";
+      document.getElementById("img").src = "";
 
-data.items.forEach(item => {
-    
-      const book = item.volumeInfo;
+ if (data.items && data.items.length > 0) {
+      
+        const book = data.items[0].volumeInfo;
             document.getElementById("Title").innerText = book.title
             document.getElementById("Author").innerText =  book.authors ? book.authors.join(", ") : "Autor não disponível"
             document.getElementById("Description").innerText = book.description || "Descrição não disponível"
             document.getElementById("img").src = book.imageLinks ? book.imageLinks.thumbnail : "Imagem não disponível"
-})
-                });
-
-            
+ }})
+.catch(error => {
+        console.error('Erro ao buscar os dados:', error);
+      })
+    })
+  
