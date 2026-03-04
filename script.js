013e1ea668eda2a366642bf703e19533043b1447
Lucas <luzlucas22022007@gmail.com>
2026-03-03 23:13:35 -0300

    style: Using CSS to improve the design

--- script.js
+++ script.js
@@ -14,19 +14,40 @@ let query = textInput.value;
     console.log(data);
      if (data.docs && data.docs.length > 0) {
       
-        const book = data.docs[0];
+        const book = data.docs[0]; //
         document.getElementById("Title").innerText = book.title ? book.title : "Título não disponível"
   document.getElementById("Author").innerText = book.author_name ?  book.author_name.join(", ") : "Autor não disponível"
+  // document.getElementById("Description").innerText = book.description ? book.description.value : "Sinopse não disponível"
   document.getElementById("img").src =
   book.cover_i
     ? `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`
     : "Imagem não encontrada"
     console.log(book.cover_i)
+     fetch(`https://openlibrary.org${book.key}.json`)
+  .then(response => response.json())
+  .then(workData => {
+    const description =
+    // verifica se a string é um objeto ou um texto comum, daí faz como um if simplificado, se for texto comum pega o texto, se for objeto pega o seu valor
+      typeof workData.description === "string"
+        ? workData.description
+        : workData.description?.value;
+
+    document.getElementById("Description").innerText =
+      description || "Sinopse não disponível";
+      console.log(description)
+  })
+  .catch(error => {
+    console.log(error)
+  
+    document.getElementById("Description").innerText =
+      "Sinopse não disponível";
+  });
  }})
   
 .catch(error => {
         console.error('Erro ao buscar os dados:', error);
+        return "Esse livro não existe na API"
       })
     })
-    
+   
   
